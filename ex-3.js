76711993f48678d07522b0a7c46d5a0dc5b4ef6a
Tle <title1337@gmail.com>
2026-04-19 09:30:36 +0700

    feat ex3

--- ex-3.js
+++ ex-3.js
@@ -1,14 +1,14 @@
 let orders = [
-  { orderId: "A001", paymentType: "Cash", totalPrice: 2500 },
-  { orderId: "A002", paymentType: "Credit Card", totalPrice: 4000 },
-  { orderId: "A003", paymentType: "Credit Card", totalPrice: 6000 },
+  { orderId: 'A001', paymentType: 'Cash', totalPrice: 2500 },
+  { orderId: 'A002', paymentType: 'Credit Card', totalPrice: 4000 },
+  { orderId: 'A003', paymentType: 'Credit Card', totalPrice: 6000 },
 ];
 
 let additionalOrders = [
-  { orderId: "A004", paymentType: "Credit Card", totalPrice: 611000 },
-  { orderId: "A005", paymentType: "Credit Card", totalPrice: 45000 },
+  { orderId: 'A004', paymentType: 'Credit Card', totalPrice: 611000 },
+  { orderId: 'A005', paymentType: 'Credit Card', totalPrice: 45000 },
 ];
 
-// Start coding here
+const newOrders = { ...orders, ...additionalOrders };
 
 console.log(newOrders);
