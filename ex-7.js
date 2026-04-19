c8d2d73b3586fec1a6fc6e2da7a099bd9faa1406
Tle <title1337@gmail.com>
2026-04-19 10:05:03 +0700

    feat ex7

--- ex-7.js
+++ ex-7.js
@@ -1,16 +1,18 @@
 const employee1 = {
-  name: "John",
+  name: 'John',
   age: 20,
 };
 
-const result1 = employee1.scores.english;
+const result1 = employee1.scores?.english;
 
 const employee2 = {
-  name: "A",
+  name: 'A',
   age: 50,
   scores: {
     math: 40,
   },
 };
 
-const result2 = employee2.scores.english;
+const result2 = employee2.scores?.english ?? 'English score is not defined';
+console.log(result1);
+console.log(result2);
